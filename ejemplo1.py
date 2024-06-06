7d9dcd20ee49d9a15979e19d1a9e4e82e0a7a3ef
Gerwmyr <2023-40129@liceocanadiense.edu.gt>
2024-06-06 14:12:03 -0600

    se van a subir 3 proyectos

---
+++ ejemplo1.py
@@ -0,0 +1,18 @@
+a=50
+b=10
+c=5
+d=4
+e=2
+suma=a+b+c+d+e
+resta=a-b-c-d-e
+multi=a*b*c*d*e
+division=a/b/c/d/e
+print("el valor de a es", a)
+print("el valor de b es", b)
+print("el valor de c es", c)
+print("el valor de d es", d)
+print("el valor de e es", e)
+print("la suma es de",suma)
+print("la resta es de",resta)
+print("la multiplicacion es de",multi)
+print("la division es de",division)
