53533c4c87907232e9ca3df9f4d51e494add64ad
Gerwmyr <2023-40129@liceocanadiense.edu.gt>
2024-06-07 12:42:29 -0600

    ejercicio 2

---
+++ ejemplo2.py
@@ -0,0 +1,13 @@
+n1=int (input("ingrese su primer dato"))
+n2=int (input("ingrese su segundo dato")) 
+n3=int (input("ingrese su tercero dato")) 
+n4=int (input("ingrese su cuarto dato")) 
+n5=int (input("ingrese su quinto dato")) 
+suma=n1+n2+n3+n4+n5 
+resta=n1-n2-n3-n4-n5 
+multiplicacion=n1*n2*n3*n4*n5 
+division=n1/n2/n3/n4/n5 
+print ("la suma de los datos es ;"+ str (suma))
+print ("la resta de los datos es ;"+ str (resta))
+print ("la multiplicacion de los datos es ;"+ str (multiplicacion))
+print ("la division de los datos es ;"+ str (division))
